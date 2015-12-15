5108450279f38e15d4c08614c3d2d8b25791f594
bfrager <btfrager@gmail.com>
2015-12-14 17:56:09 -0800

    third person fighting animation and key controls

---
+++ Assets/Scripts_Custom/FightingBehavior.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var anim: Animator;
+
+//var jumpHash : int = Animation.StringToHash("Jump");
+//var leftPunchHash : int = Animation.StringToHash("Attack.Box1_M3_Left_Jab");
+
+
+function Start () {
+	anim = GetComponent ("Animator");
+}
+
+function Update () {
+	anim.SetBool("Blocking", false);
+	if (Input.GetKeyDown("1")){
+		anim.Play("Box1_M3_Left_Jab");
+	}
+	if (Input.GetKeyDown("2")){
+		anim.Play("Box1_M3_Right_Cross");
+	}
+	if (Input.GetKey("3")){
+		anim.SetBool("Blocking", true);
+	}
+}
